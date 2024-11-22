6249bdea30a9d94b1f0c96005b9841857f30851c
Amir Dhahri <amirdhahri01@gmail.com>
2024-11-23 00:41:56 +0100

    An update

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils"
+import { getStringInfo, toUpperCase } from "../app/Utils"
 
 describe("Utils test suite" , () => {
     it("Should return uppercase of a valid string" , () => {
@@ -10,4 +10,30 @@ describe("Utils test suite" , () => {
         //Assert : 
         expect(actual).toBe(expected)
     })
+
+    it.only("Should return info for a valid string" , () => {
+
+        const actual = getStringInfo("My-String");
+
+        expect(actual.lowerCase).toBe("my-string")
+        expect(actual.upperCase).toBe("MY-STRING")
+        expect(actual.length).toBe(9)
+        expect(actual.characters.length).toBe(9)
+        expect(actual.characters).toEqual(["M","y","-","S","t","r","i","n","g"])
+        expect(actual.characters).toContain<string>("M")
+        expect(actual.extraInfo).toEqual({})
+        expect(actual.characters).toEqual(expect.arrayContaining(["i","n","g","-","S","t","r","M","y"]))
+        expect(actual.extraInfo).not.toBe(undefined)
+        expect(actual.extraInfo).toBeUndefined();
+        expect(actual.extraInfo).toBeDefined();
+        expect(actual.extraInfo).toBeTruthy();
+    })
 })
+
+// export type stringInfo = {
+//     lowerCase : string,
+//     upperCase : string,
+//     characters : string [],
+//     length : number,
+//     extraInfo : Object | undefined
+// }
